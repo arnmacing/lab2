1ea4306b61104a6a17564b58b70cd583da0c530c
arnmacing <93253694+arnmacing@users.noreply.github.com>
2021-10-29 10:53:12 +0300

    Update Furret.java

--- src/com/company/pokemons/Furret.java
+++ src/com/company/pokemons/Furret.java
@@ -3,7 +3,7 @@ package com.company.pokemons;
 import ru.ifmo.se.pokemon.*;
 import com.company.actions.*;
 
-public class Furret extends Pokemon  {
+public class Furret extends Sentret  {
     public Furret(String name,int level){
         super(name,level);
         setType(Type.NORMAL);
